9069fbac2db61445b119e2ddfe1e6d0d01b5f56d
azure provisioned user <strollback@linux-dev.strollback-linux-dev.a4.internal.cloudapp.net>
2014-01-17 21:14:48 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsstand::Application.config.secret_token = '4ffc2618ba32e4495e2cd7af3762387e08fb3cf60038039def833e5f4a40997acc4cef15d5a7e49abdf531664a39d9bb54cc472d64dcc069058e81a4ff00e56e'
